d817c45d9b8bfef88d766309d03c6c2bee16439d
TTThanhGiang <t90adidas16@gmail.com>
2024-05-12 21:37:00 +0700

    add sp vào cart và các action +, -, xóa

--- ThanhGiang_WebCuoiKi/Controllers/HomeController.cs
+++ ThanhGiang_WebCuoiKi/Controllers/HomeController.cs
@@ -108,5 +108,77 @@ namespace ThanhGiang_WebCuoiKi.Controllers
             }
             return View();
         }
+        
+        public ActionResult AddToCart(int masanpham, int? soluong)
+        {
+            var sanpham = db.tbSANPHAMs.FirstOrDefault(sp => sp.MASANPHAM == masanpham);
+            if(sanpham != null)
+            {
+                List<ModelSanpham> cart;
+                if(HttpContext.Session["Cart"] == null)
+                {
+                    cart = new List<ModelSanpham>();
+                }
+                else
+                {
+                    cart = (List<ModelSanpham>)HttpContext.Session["Cart"];
+                }
+                var spdatontai = cart.Where(sp => sp.MaSanPham == masanpham).FirstOrDefault();
+                if(spdatontai != null)
+                {
+                    if(soluong.HasValue && soluong == 0)
+                    {
+                        cart.Remove(spdatontai);
+                    }
+                    else
+                    {
+                        spdatontai.SoLuong = Convert.ToInt32(!soluong.HasValue ? 1 : soluong);
+                    }
+                }
+                else
+                {
+                    cart.Add(new ModelSanpham
+                    {
+                        MaSanPham = sanpham.MASANPHAM,
+                        TenSanPham = sanpham.TENSANPHAM,
+                        Gia = Convert.ToDecimal(sanpham.DONGIA),
+                        HinhAnh = sanpham.HINHANH,
+                        SoLuong = Convert.ToInt32(!soluong.HasValue ? 1 : soluong),
+                        MoTa = sanpham.MOTA
+                    });
+                }
+                HttpContext.Session["Cart"] = cart;
+            }
+
+            return RedirectToAction("Cart");
+        }
+        public ActionResult Cart()
+        {
+
+            List<ModelSanpham> cart = HttpContext.Session["Cart"] != null ? (List<ModelSanpham>)HttpContext.Session["Cart"] : new List<ModelSanpham>();
+            decimal TOTAL_PRICE = 0;
+            foreach (ModelSanpham p in cart)
+            {
+                TOTAL_PRICE += p.Gia * p.SoLuong;
+            }
+            ViewBag.TOTAL_PRICE = TOTAL_PRICE;
+            return View(cart);
+        }
+        public ActionResult DeleteCartItem(int masanpham)
+        {
+            List<ModelSanpham> cart;
+            cart = (List<ModelSanpham>)HttpContext.Session["Cart"];
+            if (cart != null)
+            {
+                var productToRemove = cart.FirstOrDefault(p => p.MaSanPham == masanpham);
+                if (productToRemove != null)
+                {
+                    cart.Remove(productToRemove);
+                    // cập nhật lại
+                    Session["Cart"] = cart;
+                }
+            }
+            return RedirectToAction("Cart");
+        }
     }
 }
---
+++ ThanhGiang_WebCuoiKi/Models/ModelSanpham.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThanhGiang_WebCuoiKi.Models
+{
+    public class ModelSanpham
+    {
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public string MoTa { get; set; }
+        public decimal Gia { get; set; }
+        public string HinhAnh { get; set; }
+        public int SoLuong { set; get; }
+    }
+}
