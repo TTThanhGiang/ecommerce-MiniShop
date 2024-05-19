e285b498ed7310a6604bb02c0ab9d1e3faa3c8f8
TTThanhGiang <t90adidas16@gmail.com>
2024-05-19 20:47:28 +0700

    Update Giao diện thêm sửa xóa bài đăng

--- ThanhGiang_WebCuoiKi/Controllers/BaiDangController.cs
+++ ThanhGiang_WebCuoiKi/Controllers/BaiDangController.cs
@@ -34,25 +34,50 @@ namespace ThanhGiang_WebCuoiKi.Controllers
         public ActionResult Create()
         {
             ViewBag.MACHUYENMUC = new SelectList(db.tbCHUYENMUCs, "MACHUYENMUC", "TENCHUYENMUC");
-            ViewBag.NGUOIDANG = new SelectList(db.tbNHANVIENs, "MANHANVIEN", "HOTEN");
             return View();
         }
 
         // POST: BaiDang/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "MABAIDANG,TIEUDE,NOIDUNG,HINHANH,NGUOIDANG,NGAYDANG,NGAYBATDAU,NGAYKETTHUC,MACHUYENMUC")] tbBAIDANG tbBAIDANG)
+        public async Task<ActionResult> Create([Bind(Include = "MABAIDANG,TIEUDE,NOIDUNG,NGAYDANG,NGAYBATDAU,NGAYKETTHUC,MACHUYENMUC")] tbBAIDANG tbBAIDANG, HttpPostedFileBase HINHANH)
         {
+            tbNGUOIDUNG nguoidung = (tbNGUOIDUNG)Session["NguoiDung"];
             if (ModelState.IsValid)
             {
-
-                db.tbBAIDANGs.Add(tbBAIDANG);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                if(tbBAIDANG.TIEUDE.Length > 20 && tbBAIDANG.TIEUDE.Length < 150 && tbBAIDANG.NOIDUNG != null)
+                {
+                    if (HINHANH != null && HINHANH.ContentLength > 0)
+                    {
+                        // Lưu hình ảnh mới vào thư mục trên máy chủ
+                        var fileName = Path.GetFileName(HINHANH.FileName);
+                        var path = Path.Combine(Server.MapPath("~/img/blog"), fileName);
+                        HINHANH.SaveAs(path);
+                        // Cập nhật tên hình ảnh vào cơ sở dữ liệu
+                        tbBAIDANG.HINHANH = fileName;
+                    }
+                    else
+                    {
+                        // Giữ nguyên hình ảnh cũ nếu không có tệp mới
+                        tbBAIDANG.HINHANH = " ";
+                    }
+                    tbBAIDANG.NGUOIDANG = nguoidung.MANGUOIDUNG;
+                    db.tbBAIDANGs.Add(tbBAIDANG);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    if (tbBAIDANG.TIEUDE.Length < 20 || tbBAIDANG.TIEUDE.Length > 150)
+                        ViewBag.loitieude = "Tiêu đề phải nhiều hơn 20 và nhỏ hơn 150 kí tự";
+                    if (tbBAIDANG.NOIDUNG == null)
+                        ViewBag.loinoidung = "Phải nhập nội dung";
+                    ViewBag.MACHUYENMUC = new SelectList(db.tbCHUYENMUCs, "MACHUYENMUC", "TENCHUYENMUC", tbBAIDANG.MACHUYENMUC);
+                    return View(tbBAIDANG);
+                }
             }
 
             ViewBag.MACHUYENMUC = new SelectList(db.tbCHUYENMUCs, "MACHUYENMUC", "TENCHUYENMUC", tbBAIDANG.MACHUYENMUC);
-            ViewBag.NGUOIDANG = new SelectList(db.tbNHANVIENs, "MANHANVIEN", "HOTEN", tbBAIDANG.NGUOIDANG);
             return View(tbBAIDANG);
         }
 
@@ -115,14 +140,6 @@ namespace ThanhGiang_WebCuoiKi.Controllers
             ViewBag.MACHUYENMUC = new SelectList(db.tbCHUYENMUCs, "MACHUYENMUC", "TENCHUYENMUC", tbBAIDANG.MACHUYENMUC);
             ViewBag.NGUOIDANG = new SelectList(db.tbNHANVIENs, "MANHANVIEN", "HOTEN", tbBAIDANG.NGUOIDANG);
             return View(tbBAIDANG);
-        }
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
         public ActionResult GetBaiDang(int? chuyenmuc, string timkiem)
         {
